44fa7a4860bcc50e0b55ed0f45b282dd64d0957d
John Doe <cormil@gmail.com>
2018-01-21 20:19:45 -0600

    started hw3

---
+++ hw3/BST403.java
@@ -0,0 +1,261 @@
+package hw3;
+/**
+ * Version 1.0
+ * 
+ * Your homework is to complete the methods marked ToDoX.
+ * You must not change the declaration of any method.
+ */
+
+
+/**
+ *  The B(inary)S(earch)T(ree) class represents a symbol table of
+ *  generic key-value pairs.  It supports put, get, and delete methods.
+ *  
+ *  the book's recursive versions of get and put have been renamed 
+ *  rGet  and rPut 
+ *  to facilitate testing of your non-recursive versions
+ *  
+ */
+public class BST403<Key extends Comparable<Key>, Value> {
+	private Node root;             // root of BST
+
+	private class Node {
+		private Key key;           // sorted by key
+		private Value val;         // associated data
+		private Node left, right;  // left and right subtrees
+		
+		public Node(Key key, Value val) {
+			this.key = key;
+			this.val = val;
+		}
+		
+		/**
+		 * Appends the preorder string representation of the sub-tree to the given StringBuilder.
+		 */
+		public void buildString(StringBuilder s) {
+			s.append(left == null ? '[' : '(');
+			s.append(key + "," + val);
+			s.append(right == null ? ']' : ')');
+			if (left != null) left.buildString(s);
+			if (right != null) right.buildString(s);
+		}
+	}
+	/**
+	 * Returns the preorder string representation of the BST.
+	 */
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		root.buildString(s);
+		return s.toString();
+	}
+	
+	/**
+	 * Initializes an empty symbol table.
+	 */
+	public BST403() {
+	}
+	
+	/* 
+	 * return the size of the tree
+	 */
+	public int size() {
+		
+		return -1; // ToDo 0
+	}
+	/**
+	 * Returns the value associated with the given key.
+	 * Returns null if the key is not in the table
+	 * 
+	 * ToDo 1   write a non-recursive implementation of get
+	 * 
+	 */
+	public Value get(Key key) {
+		
+		if (root == null) {
+			return null;
+		}
+		
+		Node x = root;
+		
+		while (x != null){
+			
+			int cmp = key.compareTo(x.key);
+			if (cmp > 0) { 
+				if( x.right == null) {
+					return null;
+				}
+				x = x.right;
+			}
+			if (cmp < 0) { 
+				if (x.left == null) {
+					return null;
+				}
+				x = x.left;}
+			if (cmp == 0) {
+				return x.val;
+			}
+			
+		}
+		return null; //TODO 1
+	}
+	
+	/**
+	 * Inserts the specified key-value pair into the symbol table, overwriting the old 
+	 * value with the new value if the symbol table already contains the specified key.
+	 * 
+	 * ToDo 2   write a non-recursive implementation of put
+	 * 
+	 * 
+	 */
+	public void put(Key key, Value val) {
+		
+		if (root == null) {
+			root = new Node(key, val);
+			return;
+		}
+		
+		Node x = root;
+		
+		while (x != null){
+			
+			int cmp = key.compareTo(x.key);
+			if (cmp > 0) { 
+				if( x.right == null) {
+					x.right = new Node(key, val);
+					return;
+				}
+				x = x.right;
+			}
+			if (cmp < 0) { 
+				if (x.left == null) {
+					x.left = new Node(key, val);
+					return;
+				}
+				x = x.left;}
+			if (cmp == 0) {
+				x.val = val;
+				return;
+			}
+			
+		}
+		return;  // ToDo 2
+	}
+	
+	/**
+	 * deletes the key (&value) from the table if the key is present
+	 * using the the dual of the Hibbard approach from the text. That is, 
+	 * for the two-child scenario, delete the node by replacing it 
+	 * with it's predecessor (instead of its successor)
+	 * 
+	 * ToDo 3:  implement a version of delete meeting the above spec
+	 * 
+	 */
+	public void delete(Key key, Value val) {
+		return;  // ToDo 3
+	}
+	
+	
+	/*
+	 * equals determines if two BST403s are exactly the same:
+	 * same key-value pairs, same structure
+	 * recursion might be a good choice
+	 * 
+	 * NOT ToDo , but maybe think about how you would do it.
+	 * 
+	
+	public boolean equals(BST403<Key,Value> x) {
+		return false;  
+	}
+	*/
+
+	/**
+	 * Returns the number of leaf nodes in the tree
+	 * 
+	 * ToDo 4
+	 */
+	
+	public int rNumLeaves(Node root) {
+		Integer count = 0;
+		Node x = root;
+		
+		if (x.left != null) {
+			count = rNumLeaves(x.left) + count;
+			
+			if (x.right != null) {
+				count = rNumLeaves(x.right) + count;
+			}
+		}
+		
+		if (x.left == null && x.right != null) {
+			count = rNumLeaves(x.right) + count;
+		}
+		
+		if (x.left == null && x.right == null) {
+			return 1;
+		}
+		
+		return count;
+	}
+	
+	
+	public int numLeaves() {
+		
+		Node x = root;
+		Integer count = 0;
+		
+		if (x == null ) return 0;
+		
+		return rNumLeaves(x); // ToDo 4
+	}
+	
+	/**
+	 * Returns the length of the shortest path from root to a null node.
+	 * 
+	 * ToDo 5
+	 */
+	public int lenShortestPathToNull() {
+		return 0; // ToDo 5
+	}
+	
+	/**
+	 * Verifies that 'this' is a valid binary search tree
+	 * useful to verify that your version of delete works correctly
+	 * ToDo 6
+	 * 
+	 */
+	public boolean isValidBST() {
+		return false; //ToDo 6
+	}
+	
+	/*****************************************************
+	 * 
+	 * Utility functions 
+	 */
+	
+	
+	public Value rGet(Key key) {
+		return rGet(root, key);
+	}
+	private Value rGet(Node x, Key key) {
+		if (x == null) return null;
+		int cmp = key.compareTo(x.key);
+		if      (cmp < 0) return rGet(x.left, key);
+		else if (cmp > 0) return rGet(x.right, key);
+		else              return x.val;
+	}
+	
+	public void rPut(Key key, Value val) {
+		if (key == null) throw new NullPointerException("first argument to put() is null");
+		root = rPut(root, key, val);
+	}
+	
+	private Node rPut(Node x, Key key, Value val) {
+		if (x == null) return new Node(key, val);
+		int cmp = key.compareTo(x.key);
+		if      (cmp < 0) x.left  = rPut(x.left,  key, val);
+		else if (cmp > 0) x.right = rPut(x.right, key, val);
+		else              x.val   = val;
+		return x;
+	}
+
+}
---
+++ hw3/hw3Driver.java
@@ -0,0 +1,118 @@
+/* CSC 403
+ * 
+ * Your name goes here
+ * 
+ * Get help from anyone?  put that here
+ * 
+ * Homework 1 Driver 
+ * 
+ * Instructions:  using sizeTest  as a template, create additional functions to test
+ *                the member functions in your LinkedListST implementation.
+ *                AND
+ *                create a reasonable set of test cases for each; 
+ *                call your testing functions from main
+ *                
+ */
+package hw3;
+
+import stdlib.StdIn;
+import stdlib.StdOut;
+
+public class hw3Driver {
+
+		public static void main(String[] args)
+		{
+
+				
+			//  label each test case with a comment describing what you are testing for.
+//			
+//			sizeTest("",0);				// test size on an empty ST (symbol table)
+//			sizeTest("abcde",5);        // test size on a non-empty ST
+//			putTest("abcde", "01234", "f", "5", "abcdef");
+			
+//			// middle key test
+//			getTest("abcde", "01234", "c", "2");
+//			// left most key test
+//			getTest("abcde", "01234", "a", "0");
+//			// right most key test
+//			getTest("abcde", "01234", "e", "4");
+//			// null test
+//			getTest("abcde", "01234", "f", "0");
+			
+//			numLeavesTest("dbeac", "31402", 3);
+//			numLeavesTest("cbaed", "21043", 2);
+//			numLeavesTest("bcdae", "21043", 2);
+//			numLeavesTest("ecdba", "21043", 2);
+//			numLeavesTest("edcba", "21043", 1);
+			numLeavesTest("hczijbd", "2104398", 3);
+
+		}
+
+		
+		public static void sizeTest( String vals, int answer ) {
+			
+			// create and populate the table from the input string vals
+			BST403<String,Integer> bst = new BST403<String,Integer>();
+			for (int i=0; i < vals.length(); i++) {
+				bst.put(vals.substring(i, i+1),i);
+			}
+			//  call the size function
+			int result = bst.size();
+			//report result
+			if ( result == answer)  // test passes
+				StdOut.format("sizeTest: Correct  String %s Answer: %d\n", vals,result);
+			else
+				StdOut.format("sizeTest: *Error*  String %s Answer: %d\n", vals,result);
+		}
+		
+		public static void putTest( String keys, String vals, String inKey, String inVal, String expected) {
+			
+			// create and populate the table from the input string vals
+			BST403<String,String> bst = new BST403<String,String>();
+			for (int i=0; i < vals.length(); i++) {
+				bst.put( (keys.substring(i, i+1)), (vals.substring(i, i+1)) );
+			}
+			
+			//report result
+			if ( true )  // test passes
+				StdOut.format("sizeTest: Correct  String %s Answer: %d\n", vals);
+			else
+				StdOut.format("sizeTest: *Error*  String %s Answer: %d\n", vals);
+		}
+		
+		public static void getTest( String keys, String vals, String getKey, String expectedVal) {
+			
+			// create and populate the table from the input string vals
+			BST403<String,String> bst = new BST403<String,String>();
+			for (int i=0; i < vals.length(); i++) {
+				bst.put( (keys.substring(i, i+1)), (vals.substring(i, i+1)) );
+			}
+			
+			String retrievedKey = bst.get(getKey);
+			
+			//report result
+			if ( expectedVal.equals(retrievedKey) )  // test passes
+				StdOut.format("getTest: Correct, the input key %s returns the expected value %s \n", getKey, expectedVal);
+			else
+				StdOut.format("getTest: *Error*  The input key %s does not return the expected value %s, it returned %s\n", getKey, expectedVal, retrievedKey);
+		}
+		
+		public static void numLeavesTest( String keys, String vals, Integer expectedVal) {
+			
+			// create and populate the table from the input string vals
+			BST403<String,String> bst = new BST403<String,String>();
+			for (int i=0; i < vals.length(); i++) {
+				bst.put( (keys.substring(i, i+1)), (vals.substring(i, i+1)) );
+			}
+			
+			Integer numberOfLeaves = bst.numLeaves();
+			
+			//report result
+			if ( expectedVal.equals(numberOfLeaves) )  // test passes
+				StdOut.format("numLeavesTest: Correct, the number of leaves in %s is = %s \n", keys, expectedVal);
+			else
+				StdOut.format("numLeavesTest: *Error*  The outputted value was %s, it should have returned %s\n", numberOfLeaves, expectedVal);
+		}
+		
+
+}
