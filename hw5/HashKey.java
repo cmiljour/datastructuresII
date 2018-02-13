1687ac4f723739fa01c069b27ae9c4c8f8681c53
cmiljour <cormil@gmail.com>
2018-02-13 09:46:04 -0600

    custom class complete, hw5

--- hw5/HashKey.java
+++ hw5/HashKey.java
@@ -3,21 +3,34 @@ package hw5;
 public class HashKey {
 	
 	private Integer number;
-	private String name;
+	private String text;
 
-	public HashKey() {
+	public HashKey(String text, int number) {
+		
+		this.text = text;
+		this.number = number;
 		// TODO Auto-generated constructor stub
 	}
 	
-//	public toString() {
-//		
-//	}
-//	
-	public int customHashCode(Integer key) {
-		return 0;
+	public String toString() {
+		return String.format("Text: %s , Number: %d", text,number);
 	}
-//	
-//	public equals () {
-//		
-//	}
+	
+	public int hashCode(String text, int number) {
+		int h = 17;
+		for (int i = 0; i < text.length(); i++) {
+			h = text.charAt(i) + 31 * h;
+		}
+		h = number + 31 * h;
+		return h;
+	}
+	
+	public boolean equals(Object x) {
+		if (x == this) return true;
+		if (x == null) return false;
+		if (x.getClass() != this.getClass()) return false;
+		HashKey that = (HashKey) x;
+		if (!((this.number == that.number) && (this.text == that.text))) return false;
+		return true;
+	}
 }
